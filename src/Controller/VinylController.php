7cc772ed8f6c78b6486b10f4bc8466c9e8a9e41a
gideonzozingao <gideonzozingao@LT2308VASGIDENZ.vodafonepng.local>
2024-11-06 14:18:45 +1000

    VinylController added

---
+++ src/Controller/VinylController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class VinylController extends AbstractController
+{
+    /**
+     * 
+     */
+    #[Route("/")]
+    public function homepage(): Response
+    {
+        return new Response('Vinlye: Hello Vinyle');
+    }
+
+    // #[Route("/browse")]
+    // public function browse(): Response
+    // {
+    //     return new Response('Vinlye: Hello Vinyle 1992');
+    // }
+
+    #[Route("/browse/{slug}")]
+    public function browseSlug(string $slug = null): Response
+    {
+        if ($slug) {
+            $title = "Genre: " . (str_replace("-", ' ', $slug));
+        } else {
+            $title = "All Genre";
+        }
+        return new Response($title);
+    }
+}
